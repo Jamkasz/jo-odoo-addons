c0f734e2f66b9e7d65634ddbe452785f88371fec
joelortiz <joelortiz@neovahealth.co.uk>
2015-10-20 17:55:40 +0100

    [IMP][WI-1187] task total and stage working hours looking at the project calendar config.

--- project_kanban_process/task_extension.py
+++ project_kanban_process/task_extension.py
@@ -2,18 +2,20 @@
 `task_extension.py` extends the `project.task` Odoo model adding
 extra functionality to provide a better kanban process experience.
 """
-from openerp import models, fields, api, _
+from openerp import models, fields, api
 from datetime import datetime as dt, timedelta as td
 from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT as dtf
 
 
 class ProjectTaskHistoryExtension(models.Model):
     """
-    Extends `project.task.history` Odoo model
+    Extends `project.task.history` Odoo model to change the ``date``
+    field to `datetime` type.
     """
     _name = 'project.task.history'
     _inherit = 'project.task.history'
-    _log_access = True
+
+    date = fields.Datetime('Date', select=True)
 
 
 class ProjectTaskExtension(models.Model):
@@ -23,8 +25,28 @@ class ProjectTaskExtension(models.Model):
     _name = 'project.task'
     _inherit = 'project.task'
 
-    total_time = fields.Integer(string='Total Elapsed Time', compute='_compute_total_time', store=False, readonly=True)
-    stage_time = fields.Integer(string='Stage Elapsed Time', compute='_compute_stage_time', store=False, readonly=True)
+    total_time = fields.Integer(string='Total Working Hours', compute='_compute_total_time', store=False, readonly=True)
+    stage_time = fields.Integer(string='Stage Working Hours', compute='_compute_stage_time', store=False, readonly=True)
+
+    @api.one
+    def _store_history(self):
+        """
+        Overwrites Odoo `_store_history` so it stores a `datetime` on
+        the ``date`` field instead of a `date`
+
+        :returns: ``True``
+        :rtype: bool
+        """
+        self.env['project.task.history'].create({
+            'task_id': self.id,
+            'remaining_hours': self.remaining_hours,
+            'planned_hours': self.planned_hours,
+            'kanban_state': self.kanban_state,
+            'type_id': self.stage_id.id,
+            'user_id': self.user_id.id,
+            'date': dt.now()
+        })
+        return True
 
     @api.one
     def _compute_total_time(self):
@@ -32,15 +54,17 @@ class ProjectTaskExtension(models.Model):
         Computes the total time elapsed between the creation date of the
         task until either the end date if exists or now, if it doesn't.
 
+        If the task is related to a project with a working hours
+        calendar, it returns only the working hours.
+
         :returns: time in hours
         :rtype: int
         """
-        if not self.date_end:
-            date_end = dt.now()
-        else:
+        if self.date_end:
             date_end = dt.strptime(self.date_end, dtf)
-        elapsed_time = (date_end - dt.strptime(self.create_date, dtf)).total_seconds()
-        self.total_time = int(elapsed_time/3600)
+        else:
+            date_end = dt.now()
+        self.total_time = self.get_working_hours(dt.strptime(self.date_start, dtf), date_end)[0]
 
     @api.one
     def _compute_stage_time(self):
@@ -49,20 +73,77 @@ class ProjectTaskExtension(models.Model):
         reached its current stage until the end date if exists or now,
         if it doesn't.
 
+        If the task is related to a project with a working hours
+        calendar, it returns only the working hours.
+
         :returns: time in hours
         :rtype: int
         """
-        elapsed_time = (dt.now() - dt.strptime(self.date_last_stage_update, dtf)).total_seconds()
-        task_history_records = self.env['project.task.history'].search([['task_id', '=', self.id]], order='id asc')
-        add_time = False
-        date_start = False
-        for th in task_history_records:
-            if add_time and isinstance(date_start, dt):
-                date_end = dt.strptime(th.create_date, dtf) if th.create_date else dt.strptime(th.date, '%Y-%m-%d')
-                elapsed_time += (date_end - date_start).total_seconds()
-            if self.stage_id.id != th.type_id.id:
-                add_time = False
-                continue
-            add_time = True
-            date_start = dt.strptime(th.create_date, dtf) if th.create_date else dt.strptime(th.date, '%Y-%m-%d')
-        self.stage_time = int(elapsed_time/3600)
+        if self.date_end:
+            date_end = dt.strptime(self.date_end, dtf)
+        else:
+            date_end = dt.now()
+        stage_id = self.stage_id.id
+        self.stage_time = self.stage_working_hours(stage_id, date_end)[0]
+
+    @api.one
+    def get_working_hours(self, date_start, date_end):
+        """
+        Computes the working hours between two given dates using the
+        calendar information from the related project.
+
+        Does not check if the dates are consistent with the task start
+        and end dates.
+
+        :param date_start: Start time
+        :type date_start: datetime
+        :param date_end: End time
+        :type date_end: datetime
+        :returns: working hours
+        :rtype: int
+        """
+        if not isinstance(date_start, dt):
+            raise models.except_orm('Attribute Error', 'expected datetime, received %s' % type(date_start))
+        if not isinstance(date_end, dt):
+            raise models.except_orm('Attribute Error', 'expected datetime, received %s' % type(date_end))
+        if self.project_id and self.project_id.resource_calendar_id:
+            return int(self.project_id.resource_calendar_id.get_working_hours(date_start, date_end)[0])
+        else:
+            return int((date_end - date_start).total_seconds()/3600)
+
+    @api.one
+    def stage_working_hours(self, stage_id, date):
+        """
+        Computes the working hours spent in a given stage until a given
+        date.
+
+        :param stage_id: `project.task.type` id
+        :type stage_id: int
+        :param date: end date
+        :type date: datetime
+        :returns: working hours
+        :rtype: int
+        """
+        if not isinstance(date, dt):
+            raise models.except_orm('Attribute Error', 'expected datetime, received %s' % type(date))
+        task_history_records = self.env['project.task.history'].search([
+            ['task_id', '=', self.id], ['date', '<=', date.strftime(dtf)]], order='date asc')
+        result = 0
+        if task_history_records:
+            add_time = False
+            date_start = False
+            for th in task_history_records:
+                if add_time and isinstance(date_start, dt):
+                    date_end = dt.strptime(th.date, dtf)
+                    result += self.get_working_hours(date_start, date_end)
+                if stage_id == th.type_id.id:
+                    add_time = True
+                    date_start = dt.strptime(th.date, dtf)
+                else:
+                    add_time = False
+            if add_time:
+                result += self.get_working_hours(date_start, date)
+        elif self.stage_id.id == stage_id:
+            date_start = dt.strptime(self.date_last_stage_update, dtf)
+            result += self.get_working_hours(date_start, date)
+        return result
